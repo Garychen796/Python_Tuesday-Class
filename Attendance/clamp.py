ff15425f409b35e1be7a2288fbf91fc939c77f3e
Garychen796 <cgy7961991@gmail.com>
2019-11-19 20:40:26 -0600

    11192019

---
+++ Attendance/clamp.py
@@ -0,0 +1,9 @@
+#clamp(value, min, max)
+
+def addInRange(val, minval, maxval):
+
+    if newval < minval: return minval
+    if newval > maxval: return maxval
+    return newval
+
+    val = addInRange(val, 0, 10)
