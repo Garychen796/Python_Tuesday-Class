6a5d655b4df90cd767acc882881bca055b9ced55
Garychen796 <cgy7961991@gmail.com>
2019-12-08 20:25:25 -0600

    Bouns_homework_1

---
+++ Assignments/Working_with_Files.py
@@ -0,0 +1,10 @@
+# Guanyu Chen
+# 0953074
+# 12/08/2019
+# MSITM 6341
+
+f = open("loading_file.txt", "r")
+print(f.readline())
+
+
+#open("C:/Users/X1 Carbon/Desktop/Tuesday_Python/InClassWork/loading_file.py, "r")
